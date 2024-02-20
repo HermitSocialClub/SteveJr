a63faef2785958a960c9c04c8bd1a561db59c6fe
hallea <halleaqp@gmail.com>
2024-02-19 19:53:07 -0800

    teleop fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Teles/ILTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Teles/ILTele.java
@@ -34,11 +34,11 @@ public class ILTele extends LinearOpMode {
     double x;
     double rx;
     double x2;
-    public static double kP = 0.15;
+    public static double kP = 0.01;
     public static double kI = 0;
-    public static double kD = 0;
+    public static double kD = 0.0001;
     public static double feedforward = 0.05;
-    //public static int target = ;
+    public static double target = 0;
 
     public static double ticks_per_deg = 752/180;
 
@@ -53,23 +53,34 @@ public class ILTele extends LinearOpMode {
         ElapsedTime opmodeRunTime = new ElapsedTime();
         drive.chains.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         drive.chains.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        drive.leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //drive.leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         waitForStart();
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
             opmodeRunTime.reset();
-            int target = drive.chains.getCurrentPosition();
+         //   int target = drive.chains.getCurrentPosition();
+
+
+
+            if (gamepad2.right_stick_y > 0){
+                target = target + 6;
+            } else if (gamepad2.right_stick_y < 0){
+                target = target - 6;
+            }
+
             controller.setTargetPosition(target);
             int armPos = drive.chains.getCurrentPosition();
-            double correction = controller.update(armPos,feedforward);
+            double correction = controller.update(armPos,target);
 
-            if (Math.abs(gamepad2.right_stick_y) < 0.1 ){
-                drive.chains.setPower((correction+feedforward));
-            } else {
-                drive.chains.setPower(gamepad2.right_stick_y*0.5);
-            }
+            drive.chains.setPower(correction);
+
+//            if (Math.abs(gamepad2.right_stick_y) < 0.1 ){
+//                drive.chains.setPower((correction+feedforward));
+//            } else {
+//                drive.chains.setPower(gamepad2.right_stick_y*0.5);
+//            }
 
 //            else if (gamepad2.a) {
 //                drive.chains.setPower(gamepad2.right_stick_y);
@@ -104,29 +115,29 @@ public class ILTele extends LinearOpMode {
 
             telemetry.update();
 
-//            drive.setWeightedDrivePower(
-//                    new Pose2d(
-//                            -(((Math.abs(gamepad1.left_stick_y) < .2) ? 0 : gamepad1.left_stick_y) / .70) * (gamepad1.right_trigger > 0.05 ? 1 : 0.5),
-//                            -(((Math.abs(gamepad1.left_stick_x) < .2) ? 0 : gamepad1.left_stick_x) / .70) * (gamepad1.right_trigger > 0.05 ? 1 : 0.5),
-//                            -(((Math.abs(gamepad1.right_stick_x) < .2) ? 0 : gamepad1.right_stick_x) / .70) * 0.7 * (gamepad1.right_trigger > 0.05 ? 1 : 0.6)
-//                    ));
+            drive.setWeightedDrivePower(
+                    new Pose2d(
+                            -(((Math.abs(gamepad1.left_stick_y) < .2) ? 0 : gamepad1.left_stick_y) / .70) * (gamepad1.right_trigger > 0.05 ? 1 : 0.5),
+                            -(((Math.abs(gamepad1.left_stick_x) < .2) ? 0 : gamepad1.left_stick_x) / .70) * (gamepad1.right_trigger > 0.05 ? 1 : 0.5),
+                            -(((Math.abs(gamepad1.right_stick_x) < .2) ? 0 : gamepad1.right_stick_x) / .70) * 0.7 * (gamepad1.right_trigger > 0.05 ? 1 : 0.6)
+                    ));
 
-            x = gamepad1.left_stick_x * 1;
-            x2 = gamepad1.left_stick_x * 1;
-            y = -gamepad1.left_stick_y;
-            rx = gamepad1.right_stick_x;
-
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double denominator2 = Math.max(Math.abs(y) + Math.abs(x2) + Math.abs(rx), 1);
-            driveRightPower = (y-x2-rx) / denominator2;
-            driveRight2Power = (y+x-rx) / denominator;
-            driveLeftPower = (y+x+rx) / denominator;
-            driveLeft2Power = (y-x2+rx) / denominator2;
-
-            drive.rightRear.setPower(driveRight2Power*0.93);
-            drive.rightFront.setPower(driveRightPower*0.98);
-            drive.leftRear.setPower(driveLeft2Power*0.93);
-            drive.leftFront.setPower(driveLeftPower);
+//            x = gamepad1.left_stick_x * 1;
+//            x2 = gamepad1.left_stick_x * 1;
+//            y = -gamepad1.left_stick_y;
+//            rx = gamepad1.right_stick_x;
+//
+////            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+////            double denominator2 = Math.max(Math.abs(y) + Math.abs(x2) + Math.abs(rx), 1);
+////            driveRightPower = (y-x2-rx) / denominator2;
+////            driveRight2Power = (y+x-rx) / denominator;
+////            driveLeftPower = (y+x+rx) / denominator;
+////            driveLeft2Power = (y-x2+rx) / denominator2;
+//
+//            drive.rightRear.setPower(driveRight2Power*0.92);
+//            drive.rightFront.setPower(driveRightPower*0.97);
+//            drive.leftRear.setPower(driveLeft2Power*0.93);
+//            drive.leftFront.setPower(driveLeftPower);
 
 
                 if (gamepad1.right_bumper) {
@@ -138,21 +149,22 @@ public class ILTele extends LinearOpMode {
                 if (gamepad2.left_trigger > 0.5){
                     drive.clawFlipper.setPosition(0.3);
                 } else if (gamepad2.right_trigger > 0.5) {
-                    drive.clawFlipper.setPosition(0.75);
+                    drive.clawLeft.setPosition(0.15);
+                    drive.clawRight.setPosition(0.8);
                 } else {
                     drive.clawFlipper.setPosition(0.87);
                 }
 
                 if (gamepad2.left_bumper){
-                    drive.clawLeft.setPosition(0.7);
-                    drive.clawRight.setPosition(0.7);
-                } else if (gamepad2.right_bumper){
                     drive.clawLeft.setPosition(0.15);
-                    drive.clawRight.setPosition(0.15);
+                    //drive.clawRight.setPosition(0.8);
+                } else if (gamepad2.right_bumper){
+                  // drive.clawLeft.setPosition(0.7);
+                    drive.clawRight.setPosition(0.8);
                 }
                 else {
-                    drive.clawLeft.setPosition(0);
-                    drive.clawRight.setPosition(0);
+                    drive.clawLeft.setPosition(0.7);
+                    drive.clawRight.setPosition(0.15);
                 }
 
 
